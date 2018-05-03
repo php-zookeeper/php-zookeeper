efc760c3e27c2fb108fb4d3948b701453c960ea9
Timandes White <timands@gmail.com>
2018-05-03 22:10:15 +0800

    Update example

--- examples/getConfig.php
+++ examples/getConfig.php
@@ -2,5 +2,12 @@
 
 $zk = new Zookeeper('localhost:2181');
 $conf = $zk->getConfig();
-$r = $conf->get();
+$r = $conf->get(function() {
+	echo "Bingo!" . PHP_EOL;
+});
 var_dump($r);
+
+while(1) {
+	zookeeper_dispatch();
+	sleep(1);
+}
