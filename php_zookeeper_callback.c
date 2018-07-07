e05602fa6baee4a309419d950ebeb7589b338f71
Timandes White <timands@gmail.com>
2018-07-07 13:49:05 +0800

    Update authors' list

--- php_zookeeper_callback.c
+++ php_zookeeper_callback.c
@@ -10,7 +10,8 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Authors: Timandes White <timands@gmail.com>                          |
+  | Authors: Andrei Zmievski <andrei@php.net>                            |
+  |          Timandes White <timands@gmail.com>                          |
   +----------------------------------------------------------------------+
 */
 
