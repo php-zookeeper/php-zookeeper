49934808bbd82589ec5fabf5b791d4a9b8ac84ad
Timandes White <timands@gmail.com>
2019-09-18 10:03:24 +0800

    chore: try to fix scripts for CI

--- .travis/install_libzookeeper.sh
+++ .travis/install_libzookeeper.sh
@@ -12,14 +12,21 @@ else
 fi
 URL_DIR_NAME=zookeeper-${LIBZOOKEEPER_VERSION}
 LIBZOOKEEPER_PREFIX=${HOME}/lib${URL_DIR_NAME}
+URL_PREFIX=http://apache.mirrors.lucidnetworks.net/zookeeper
 
 TRAVIS_SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-wget http://apache.mirrors.lucidnetworks.net/zookeeper/${URL_DIR_NAME}/${PACKAGE_NAME}.tar.gz || exit 1
+wget ${URL_PREFIX}/${URL_DIR_NAME}/${PACKAGE_NAME}.tar.gz || exit 1
 tar xvf ${PACKAGE_NAME}.tar.gz || exit 1
 
 if [ ${LIBZOOKEEPER_MAJOR_VERSION} -ge 3 -a ${LIBZOOKEEPER_MINOR_VERSION} -ge 5 ]; then
-    cd ${PACKAGE_NAME}
+	if [ ${LIBZOOKEEPER_PATCH_VERSION} -ge 5 ]; then
+		wget ${URL_PREFIX}/${URL_DIR_NAME}/${PACKAGE_NAME}-bin.tar.gz || exit 1
+		tar xvf ${PACKAGE_NAME}-bin.tar.gz || exit 1
+		cd ${PACKAGE_NAME}-bin
+	else
+        cd ${PACKAGE_NAME}
+	fi
         ${TRAVIS_SCRIPT_DIR}/init_zk_instances.sh || exit 1
         ${TRAVIS_SCRIPT_DIR}/launch_zk_instances.sh || exit 1
     cd ..
