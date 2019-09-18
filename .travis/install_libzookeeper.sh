c065f1fab1a70e6a22e2e365a0391d2873341c7c
Timandes White <timands@gmail.com>
2019-09-18 01:22:04 +0000

    chore: try to fix scripts for CI

--- .travis/install_libzookeeper.sh
+++ .travis/install_libzookeeper.sh
@@ -3,13 +3,19 @@
 LIBZOOKEEPER_VERSION=$1
 LIBZOOKEEPER_MAJOR_VERSION=`echo ${LIBZOOKEEPER_VERSION} | awk -F'.' '{print $1}'`
 LIBZOOKEEPER_MINOR_VERSION=`echo ${LIBZOOKEEPER_VERSION} | awk -F'.' '{print $2}'`
+LIBZOOKEEPER_PATCH_VERSION=`echo ${LIBZOOKEEPER_VERSION} | awk -F'.' '{print $3}'`
 
-PACKAGE_NAME=zookeeper-${LIBZOOKEEPER_VERSION}
-LIBZOOKEEPER_PREFIX=${HOME}/lib${PACKAGE_NAME}
+if [ ${LIBZOOKEEPER_MAJOR_VERSION} -ge 3 -a ${LIBZOOKEEPER_MINOR_VERSION} -ge 5 -a ${LIBZOOKEEPER_PATCH_VERSION} -ge 5 ]; then
+    PACKAGE_NAME=apache-zookeeper-${LIBZOOKEEPER_VERSION}
+else
+    PACKAGE_NAME=zookeeper-${LIBZOOKEEPER_VERSION}
+fi
+URL_DIR_NAME=zookeeper-${LIBZOOKEEPER_VERSION}
+LIBZOOKEEPER_PREFIX=${HOME}/lib${URL_DIR_NAME}
 
 TRAVIS_SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-wget http://apache.mirrors.lucidnetworks.net/zookeeper/${PACKAGE_NAME}/${PACKAGE_NAME}.tar.gz || exit 1
+wget http://apache.mirrors.lucidnetworks.net/zookeeper/${URL_DIR_NAME}/${PACKAGE_NAME}.tar.gz || exit 1
 tar xvf ${PACKAGE_NAME}.tar.gz || exit 1
 
 if [ ${LIBZOOKEEPER_MAJOR_VERSION} -ge 3 -a ${LIBZOOKEEPER_MINOR_VERSION} -ge 5 ]; then
@@ -22,7 +28,12 @@ else
     ${PACKAGE_NAME}/bin/zkServer.sh start
 fi
 
-cd ${PACKAGE_NAME}/src/c
+if [ ${LIBZOOKEEPER_MAJOR_VERSION} -ge 3 -a ${LIBZOOKEEPER_MINOR_VERSION} -ge 5 -a ${LIBZOOKEEPER_PATCH_VERSION} -ge 5 ]; then
+    cd ${PACKAGE_NAME}/zookeeper-client/zookeeper-client-c
+    autoreconf -if
+else
+    cd ${PACKAGE_NAME}/src/c
+fi
     ./configure --prefix=${LIBZOOKEEPER_PREFIX} || exit 1
     make || exit 1
     make install || exit 1
